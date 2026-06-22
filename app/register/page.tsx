1e0e6b00482ab851ac40603bde0e9bad81be3b7a
Kennyth3 <jakkapaul@gmail.com>
2026-06-22 10:30:25 +0700

    update

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,11 +1,52 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+
 export default function Register() {
+    
+    const router = useRouter();
+
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  async function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+
+    const res = await fetch("/api/auth/register", {
+      method: "POST",
+      body: JSON.stringify(form),
+    });
+
+    if (res.ok) {
+      router.push("/login");
+    } else {
+      alert("Register failed");
+    }
+  }
+
     return (
-        <div className="auth-page">
+        <div className="auth-page" onSubmit={handleSubmit}>
           <form className="auth-card">
              <h1> Register </h1> 
-             <input  placeholder="กรุณาเขียนชื่อขนามสกุล"/>
-             <input  placeholder="Email"  type="email"/>
-             <input  placeholder="Password"/>
+             <input  
+             placeholder="กรุณาเขียนชื่อขนามสกุล"
+             onChange={(e) => setForm({ ...form, name: e.target.value })}
+        />
+
+             <input  placeholder="Email" 
+              type="email"
+              onChange={(e) => setForm({ ...form, email: e.target.value })}
+        />
+
+             <input  
+              placeholder="Password"
+              onChange={(e) => setForm({ ...form, password: e.target.value })}
+        />
 
              <button> Register </button>
 
