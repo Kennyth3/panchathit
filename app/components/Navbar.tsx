d7793fb43f33a712b4d5ed5fb951002eda25dbc8
Kennyth3 <jakkapaul@gmail.com>
2026-06-29 10:24:47 +0700

    update

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,10 +1,44 @@
+
 "use client";
 
 import Link from "next/link";
-import { useState } from "react";
+
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+
+type User = {
+  name: string;
+  email: string;
+  role: "admin" | "user";
+};
 
 export default function Navbar() {
+
   const [menuOpen, setMenuOpen] = useState(false);
+
+  const router = useRouter();
+  const [user, setUser] = useState<User | null>(null);
+  // const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    async function loadUser() {
+      const res = await fetch("/api/auth/Me");
+      const data = await res.json();
+      setUser(data.user);
+    }
+
+    loadUser();
+  }, []);
+
+  async function Logout() {
+    await fetch("/api/auth/Logout", {
+      method: "POST",
+    });
+
+    setUser(null);
+    router.push("/login");
+    router.refresh();
+  }
 
   return (
     <nav className="navbar">
@@ -20,25 +54,53 @@ export default function Navbar() {
           ☰
         </button>
 
-        <ul className={menuOpen ? "nav-links active" : "nav-links"}>
+          <ul className={menuOpen ? "nav-links active" : "nav-links"}>
           <li>
             <Link href="/">หน้าหลัก</Link>
           </li>
           <li>
             <Link href="/about">เกี่ยวกับเรา</Link>
           </li>
-          <li>
-            <Link href="/dashboard">Dashboard</Link>
-          </li>
-          <li>
-            <Link href="/profile">Profile</Link>
-          </li>
-          <li>
-            <Link href="/login">Login</Link>
-          </li>
-          <li>
-            <Link href="/register">Register</Link>
-          </li>
+
+          {user && (
+            <li>
+              <Link href="/dashboard">Dashboard</Link>
+            </li>
+          )}
+
+          {user?.role === "admin" && (
+            <li>
+              <Link href="/admin/users">Admin</Link>
+            </li>
+          )}
+
+          {!user ? (
+            <>
+              <li>
+                <Link href="/login">Login</Link>
+              </li>
+              <li>
+                <Link href="/register" className="btn-register">
+                  Register
+                </Link>
+              </li>
+            </>
+          ) : (
+            <>
+              <li className="user-info">
+                {user.name} ({user.role})
+              </li>
+              <li>
+                <Link href="/profile">Profile</Link>
+              </li>
+              <li>
+                <button onClick={Logout} className="btn-logout">
+                  Logout
+                </button>
+              </li>
+            </>
+          )}
+
         </ul>
       </div>
     </nav>
