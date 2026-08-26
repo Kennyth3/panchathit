7e5e0c34e5676b58280793ab828c185479fa2326
Kennyth3 <jakkapaul@gmail.com>
2026-08-26 09:48:22 +0700

    update

--- app/Admin/blogs/page.tsx
+++ app/Admin/blogs/page.tsx
@@ -1,9 +1,12 @@
 import BlogForm from "@/components/BlogForm";
+import BlogCard from "@/components/BlogCard";
 
 export default function AdminblogsPage() {
   return (
     <main className="p-6">
       <BlogForm />
+      <BlogCard/>
     </main>
+    
   );
 }
